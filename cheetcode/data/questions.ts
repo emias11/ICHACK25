81686ff410acf348301b79d03fe1a8b39c68da19
ms1720 <ms1720@ic.ac.uk>
2025-02-02 00:47:03 +0000

    Added question .ts file

--- cheetcode/data/questions.ts
+++ cheetcode/data/questions.ts
@@ -1,28 +1,88 @@
-export const all_topics = [ "topic0", "topic1", "topic2", "topic3" ]
+export const all_topics = ["Array", "Hash Table", "Tree", "Depth-First Search", "Breadth-First Search", "Binary Tree", "Linked List", "Two Pointers", "Recursion", "Math", "Binary Search"]
 
 export const problems = [
     {
-    problem_title: "1",
-    problem_description: "This is problem 1",
-    topics: [all_topics[0], all_topics[1]],
+    problem_title: "Two Sum",
+    problem_description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.",
+    topics: ["Array", "Hash Table"],
     problem_questions: []
     },
     {
-    problem_title: "2",
-    problem_description: "This is problem 2",
-    topics: [all_topics[1], all_topics[2]],
+    problem_title: "Same Tree",
+    problem_description: "Given the roots of two binary trees p and q, write a function to check if they are the same or not. Two binary trees are considered the same if they are structurally identical, and the nodes have the same valu",
+    topics: ["Tree", "Depth-First Search", "Breadth-First Search", "Binary Tree", "Recursion"],
     problem_questions: []
     },
     {
-    problem_title: "3",
-    problem_description: "This is problem 3",
-    topics: [all_topics[2], all_topics[3]],
+    problem_title: "Binary Tree Inorder Traversal",
+    problem_description: "Given the root of a binary tree, return the inorder traversal of its nodes' values.",
+    topics: ["Stack", "Tree", "Depth-First Search", "Binary Tree"],
     problem_questions: []
     },
     {
-    problem_title: "4",
-    problem_description: "This is problem 4",
-    topics: ["not_in_all_topics"],
+    problem_title: "Path Sum",
+    problem_description: "Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum. A leaf is a node with no children.",
+    topics: ["Tree", "Depth-First Search", "Breadth-First Search", "Binary Tree"],
     problem_questions: []
-    }
+    },
+    {
+    problem_title: "Minimum Depth of Binary Tree",
+    problem_description: "The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node. Note: A leaf is a node with no children.",
+    topics: ["Tree", "Depth-First Search", "Breadth-First Search", "Binary Tree"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Rotate List",
+    problem_description: "Given the head of a linked list, rotate the list to the right by k places.",
+    topics: ["Linked List", "Two Pointers"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Merge Two Sorted Lists",
+    problem_description: "You are given the heads of two sorted linked lists list1 and list2. Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists. Return the head of the merged linked list.",
+    topics: ["Linked List", "Recursion"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Remove Duplicates from Sorted List",
+    problem_description: "Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.",
+    topics: ["Linked List"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Check if Array is Sorted and Rotated",
+    problem_description: "Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false. There may be duplicates in the original array.",
+    topics: ["Array", "Recursion"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Reverse Linked List",
+    problem_description: "Given the head of a singly linked list, reverse the list, and return the reversed list.",
+    topics: ["Array", "Recursion"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Power of Three",
+    problem_description: "Given an integer n, return true if it is a power of three. Otherwise, return false. An integer n is a power of three, if there exists an integer x such that n == 3x.",
+    topics: ["Math", "Recursion"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Sqrt(x)",
+    problem_description: "Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well. You must not use any built-in exponent function or operator.",
+    topics: ["Math", "Binary Search"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Count Complete Tree Nodes",
+    problem_description: "Given the root of a complete binary tree, return the number of the nodes in the tree. According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.",
+    topics: ["Binary Search", "Tree", "Binary Tree"],
+    problem_questions: []
+    },
+    {
+    problem_title: "Search Insert Position",
+    problem_description: "Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.",
+    topics: ["Binary Search", "Array"],
+    problem_questions: []
+    },
 ]
